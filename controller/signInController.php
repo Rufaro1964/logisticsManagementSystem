a01cd9f6d3b9db6075d10515788e5120ba4fe8e3
DalitsoTembo <tembodalitso2@gmail.com>
2021-07-12 11:11:28 +0200

    Directory structure and other additions

---
+++ controller/signInController.php
@@ -0,0 +1,12 @@
+<?php
+
+    include('../view/sign-in.html');
+
+    
+    if(isset($_POST['submit'])){
+        if(isset($_POST['username']) && isset($_POST['password'])){
+            // Check if the user exists...
+        }
+    }
+
+?>
