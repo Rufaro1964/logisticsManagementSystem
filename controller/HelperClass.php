c1935a9595fae82794d35b866b7e51abc673d26b
DalitsoTembo <tembodalitso2@gmail.com>
2021-08-17 20:03:16 +0200

    Created Helper Classes

---
+++ controller/HelperClass.php
@@ -0,0 +1,25 @@
+<?php
+
+    class HelperClass{
+
+        public function __construct(){}
+
+
+        public function existsUser() :bool {
+
+
+            
+            return false;
+
+        }
+
+
+        
+        public function existsNextOfKin() :bool{
+
+            return false;
+        }
+
+    }
+
+?>
